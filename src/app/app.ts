b6e53d061b54fd951514cb251c801d347f8f1df8
negin-khademi <negin.khademi@gmail.com>
2025-08-04 16:43:06 +0330

    feat : import images

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from "@angular/core";
 
 import { ButtonModule } from "primeng/button";
+import { DessertsComponent } from "./desserts/desserts.component";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, DessertsComponent],
   templateUrl: "./app.html",
   styleUrl: "./app.scss",
 })
