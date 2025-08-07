50f8fe927dccbc9a947647270fa430cd31791b71
negin-khademi <negin.khademi@gmail.com>
2025-08-07 17:03:07 +0330

    feat : add dark class to project

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,5 +11,12 @@ import { RouterOutlet } from "@angular/router";
   styleUrl: "./app.scss",
 })
 export class App {
+  ngOnInit(): void {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+  }
+
   protected readonly title = signal("03-product-list");
 }
